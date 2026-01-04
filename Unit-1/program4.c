fb5a02a69d6725f2c8e42b9e143932c60790e13a
Tanveer Alam <tanveer.alam79856@gmail.com>
2026-01-04 09:01:17 +0530

    Create program4.c

---
+++ Unit-1/program4.c
@@ -0,0 +1,10 @@
+//largest of two numbers
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    scanf("%d %d",&a,&b);
+    if(a>b){printf("a is greater than b");}
+    else{printf("b is greater than a");}
+    return 0;
+}
