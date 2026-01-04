2ff970f9e15818b01f76154f1b215f1602f0049b
Tanveer Alam <tanveer.alam79856@gmail.com>
2026-01-04 09:04:58 +0530

    Create program7.c

---
+++ Unit-2/program7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter any integer:");
+    scanf("%d",&a);
+    if(a%2==0){printf("Even number");}
+    else{printf("Odd number");}
+    return 0;
+}
